50dd155ba85f384498d23fdb23dcb2d1b5d8fd1b
Sebastián Figueroa <sebastian.figueroa@parqueexplora.org>
2021-06-14 08:34:31 -0500

    Changed helloworld message

--- Lambda/info.js
+++ Lambda/info.js
@@ -1,3 +1,3 @@
 exports.handler = (event, context, callback) => {
-    return callback(null,{statusCode:200,body: "HelloWorld!"});
+    return callback(null,{statusCode:200,body: "HelloWorld!!"});
 }
